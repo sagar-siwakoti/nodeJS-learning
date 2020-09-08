f508058ebc7e83049f0e4e0390f798b43cfee201
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-08 12:50:29 +0545

    -added and updated ejs

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-const express = require("express");
-//express app
+const express = require('express');
+
+// express app
 const app = express();
-//listen for request
+
+// listen for requests
 app.listen(3000);
-app.get("/", (req, res) => {
-  //res.send('<p>This is home page</p>');
-  res.sendFile("./views/index.html", { root: __dirname });
-});
-app.get("/about", (req, res) => {
-  //res.send('<p>This is about page</p>');
-  res.sendFile("./views/about.html", { root: __dirname });
-});
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+
+// 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render('404', { title: '404' });
 });
