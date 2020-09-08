dcb8e00963e8744a5d36e380b657f63ef69c9a62
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-08 16:20:06 +0545

    -added mongoDB and mongoose

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require("express");
-const morgan=require("morgan")
-
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 // express app
 const app = express();
-// listen for requests
-app.listen(3000);
-
-//creating middleware
-app.use(express.static('public'));
-app.use(morgan('dev'))
-
+//connect to db
+const dbURI =
+  "mongodb+srv://sagar-siwakoti:test1234@nodeblogapp.agwsg.mongodb.net/node-blog-app?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set("view engine", "ejs");
 // app.set('views', 'myviews');
+
+// listen for requests
+
+
+//creating middleware
+app.use(express.static("public"));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
